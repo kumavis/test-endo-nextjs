ea85b1c7de1070440e623605afbe557920e40aa6
kumavis <aaron@kumavis.me>
2022-10-06 14:55:25 -1000

    modify ses to only run once

--- public/lockdown.js
+++ public/lockdown.js
@@ -1,13 +1,15 @@
-if (typeof lockdown !== 'undefined') {
+if (typeof lockdown !== 'undefined' && !globalThis.didLockdown) {
     lockdown({
         // see errors for debugging
         errorTaming: 'unsafe',
         // allow eval for bundle
         evalTaming: 'unsafeEval',
+        // nextjs wants to set the console
+        consoleTaming: 'unsafe',
     })
-    console.log('ayy')
-    debugger
-} else {
+    console.log('lockdown')
+    globalThis.didLockdown = true;
+} else if (!globalThis.didLockdown) {
     console.warn('no lockdown')
     debugger
 }
